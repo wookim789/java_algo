5448aafb9fac6ebf719134c2ee3dd4ee2f0c7492
wookim789 <wookim789@gmail.com>
2021-05-12 00:35:58 +0900

    1065 한수 문제 풀이

---
+++ src/backjoon/Hansoo.java
@@ -0,0 +1,31 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//public class Hansoo {
+class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        int result = 0;
+        if(0 < n && n < 100) {
+            result = n;
+        }else if (100 <= n && n <= 1000){
+            result += 99;
+            for(int i = 100; i <= n; i++){
+                int f = i / 100;
+                int s = (i % 100) / 10;
+                int t = (i % 10);
+                if(f - s == s - t){
+                    result += 1;
+                }
+            }
+        }
+
+        System.out.println(result);
+        return;
+    }
+}
