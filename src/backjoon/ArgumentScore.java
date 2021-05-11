a6d436c90c513e174729700a0820145f1fd29c5d
wookim789 <wookim789@gmail.com>
2021-05-11 23:17:56 +0900

    first commit

---
+++ src/backjoon/ArgumentScore.java
@@ -0,0 +1,22 @@
+package backjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+class ArgumentScore {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        float n = Integer.parseInt(br.readLine());
+        List<Float> scores = Arrays.stream(br.readLine().split(" "))
+                .map(Float::parseFloat).collect(Collectors.toList());
+
+        Optional<Float> optMaxV = scores.stream().reduce(Float::max);
+        Optional<Float> optSum = scores.stream().reduce(Float::sum).map(x->x/n);
+        if(optSum.isPresent() && optMaxV.isPresent())
+            System.out.println(optSum.get() / optMaxV.get() * 100.0);
+    }
+}
---
+++ src/backjoon/ScoreAverage.java
@@ -0,0 +1,31 @@
+package backjoon;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class ScoreAverage {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<String> arr = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            arr.add(br.readLine());
+        }
+
+        arr.forEach(x->{
+            List<Float> scoreList = Arrays.stream(x.split(" "))
+                    .map(Float::parseFloat).collect(Collectors.toList());
+
+            float m = scoreList.get(0);
+            float avg = scoreList.stream().reduce(Float::sum).map(y->(y-m)/m).get();
+            float cnt = scoreList.stream().filter(score -> score > avg).count();
+
+            System.out.println(String.format("%.3f", cnt/m * 100f) + "%");
+        });
+    }
+}
