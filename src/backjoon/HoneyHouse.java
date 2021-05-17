e7a3e0e554b9d6bc7536300394d54298abf4c46b
wookim789 <wookim789@gmail.com>
2021-05-17 23:44:07 +0900

    master 벌집 풀이중

--- src/backjoon/HoneyHouse.java
+++ src/backjoon/HoneyHouse.java
@@ -1,4 +1,20 @@
 package backjoon;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class HoneyHouse {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m  = n / 6;
+        int g = n % 6;
+
+        if(g != 0){
+            m ++;
+        }
+        System.out.println(m);
+    }
 }
