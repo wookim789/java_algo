85477bb3cca6449f318ce45471615e6ea4faff57
wookim789 <wookim789@gmail.com>
2021-06-05 23:04:23 +0900

    큰수 합산 풀이 완료

---
+++ src/backjoon/SumBigInt.java
@@ -0,0 +1,18 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SumBigInt {
+    public static void main(String [] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<BigInteger> arr = Arrays.stream(br.readLine().split(" ")).map(s->new BigInteger(s)).collect(Collectors.toList());
+
+        System.out.println(arr.get(0).add(arr.get(1)));
+    }
+}
