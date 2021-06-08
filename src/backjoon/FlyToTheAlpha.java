a8bae02c2da985d355ac870ca4f48730c9dc8d8d
wookim789 <wookim789@gmail.com>
2021-06-08 23:52:29 +0900

    fly to the alpha centauri 풀이 중

--- src/backjoon/FlyToTheAlpha.java
+++ src/backjoon/FlyToTheAlpha.java
@@ -1,37 +1,40 @@
 package backjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 //public class Main {
 public class FlyToTheAlpha {
     public static void main(String[] args) throws Exception{
-        // k - 1, k, k + 1
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
         while (t > 0){
-            int st = Integer.parseInt(sc.next());
-            int ed = Integer.parseInt(sc.next());
-            int k = 0;
-            int distance = st;
-            int result = 0;
-            boolean isAccelerate = true;
+            List<Integer> list = Arrays.stream(br.readLine().split(" "))
+                                    .map(Integer::parseInt)
+                                    .collect(Collectors.toList());
 
-            while(distance < ed){
-                if(k * (k+1)/2 < (ed-st)/2 && isAccelerate){
+            int st = list.get(0), ed = list.get(1), k = 0, result = 0, dist = 0, bet = ed - st;
+
+            while(true){
+                if(k * (k+1)/2 < (bet)/2 ){
                     k++;
-                }else if(k > 1){
-                    isAccelerate = false;
-                    k--;
+                }else if(result == 0 && dist == 0){
+                    result = 2 * k - 1;
+                    dist = (k-1) * k/2 + k * (k+1)/2 + st;
+                }else if(dist == ed){
+                    break;
+                }else {
+                    result ++;
+                    dist ++;
                 }
-//                System.out.println(k + " " + distance);
-                distance += k;
-                result++;
             }
-            System.out.println(result);
             t--;
-//            System.out.println();
+            System.out.println(result);
         }
     }
 }
