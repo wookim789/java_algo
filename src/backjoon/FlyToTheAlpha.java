b6f74c348cee19f8b6c72ca9495d3ee72bf8c86c
wookim789 <wookim789@gmail.com>
2021-06-09 20:43:46 +0900

    fly to the alpha centauri 풀이 중

--- src/backjoon/FlyToTheAlpha.java
+++ src/backjoon/FlyToTheAlpha.java
@@ -2,9 +2,9 @@ package backjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 //public class Main {
@@ -18,19 +18,36 @@ public class FlyToTheAlpha {
                                     .map(Integer::parseInt)
                                     .collect(Collectors.toList());
 
-            int st = list.get(0), ed = list.get(1), k = 0, result = 0, dist = 0, bet = ed - st;
+            int st = list.get(0), ed = list.get(1), k = 0, result = 0, dist = 0, avg = (ed - st)/2;
+            List<Integer> dpTable = new ArrayList<>();
 
             while(true){
-                if(k * (k+1)/2 < (bet)/2 ){
-                    k++;
-                }else if(result == 0 && dist == 0){
-                    result = 2 * k - 1;
-                    dist = (k-1) * k/2 + k * (k+1)/2 + st;
-                }else if(dist == ed){
-                    break;
-                }else {
-                    result ++;
-                    dist ++;
+                if(dpTable.size() == k){
+                    if(dist < avg){
+                        k++;
+                        dist += k;
+                        dpTable.add(dist);
+                    }else if(result == 0){
+                        result = 2 * k - 1;
+                        dist = (k-1) * k/2 + dist;
+                    }else if(st + dist == ed){
+                        break;
+                    }else {
+                        result ++;
+                        dist ++;
+                    }
+                }else{
+                    if(dpTable.get(k-1) < avg){
+                        k++;
+                    }else if(result == 0){
+                        result = 2 * k - 1;
+                        dist = (k-1) * k/2 + dpTable.get(k);
+                    }else if(st + dist == ed){
+                        break;
+                    }else {
+                        result ++;
+                        dist ++;
+                    }
                 }
             }
             t--;
