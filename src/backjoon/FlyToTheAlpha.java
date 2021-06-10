22472ec9c8770c8583015c840e9177fd1f9be06a
wookim789 <wookim789@gmail.com>
2021-06-10 22:43:15 +0900

    fly to the alpha centauri 다음에 다시 도전

--- src/backjoon/FlyToTheAlpha.java
+++ src/backjoon/FlyToTheAlpha.java
@@ -13,45 +13,15 @@ public class FlyToTheAlpha {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        while (t > 0){
+        while (t > 0) {
             List<Integer> list = Arrays.stream(br.readLine().split(" "))
-                                    .map(Integer::parseInt)
-                                    .collect(Collectors.toList());
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
 
-            int st = list.get(0), ed = list.get(1), k = 0, result = 0, dist = 0, avg = (ed - st)/2;
+            int st = list.get(0);
+            int ed = list.get(1);
+
             List<Integer> dpTable = new ArrayList<>();
-
-            while(true){
-                if(dpTable.size() == k){
-                    if(dist < avg){
-                        k++;
-                        dist += k;
-                        dpTable.add(dist);
-                    }else if(result == 0){
-                        result = 2 * k - 1;
-                        dist = (k-1) * k/2 + dist;
-                    }else if(st + dist == ed){
-                        break;
-                    }else {
-                        result ++;
-                        dist ++;
-                    }
-                }else{
-                    if(dpTable.get(k-1) < avg){
-                        k++;
-                    }else if(result == 0){
-                        result = 2 * k - 1;
-                        dist = (k-1) * k/2 + dpTable.get(k);
-                    }else if(st + dist == ed){
-                        break;
-                    }else {
-                        result ++;
-                        dist ++;
-                    }
-                }
-            }
-            t--;
-            System.out.println(result);
         }
     }
 }
