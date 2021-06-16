19a2795dbc76296c873481c3597b10aa74559601
wookim789 <wookim789@gmail.com>
2021-06-16 23:08:13 +0900

    4948 문제 풀이
    - 에레테네토스의 채 문제
    - 배열 활용 능력
    - 문제를 자세히 읽어 경계 조건 캐치 능력
    - buffered 툴 사용 능력

---
+++ src/backjoon/mathtwo/Eretenethos4.java
@@ -0,0 +1,58 @@
+package backjoon.mathtwo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+//public class Eretenethos4 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> nList = new ArrayList<>();
+
+        int i = Integer.parseInt(br.readLine()), max = 0;
+
+        while(i != 0){
+            if(i > max){
+                max = i;
+            }
+            nList.add(i);
+            i = Integer.parseInt(br.readLine());
+        }
+        br.close();
+
+        max = max * 2;
+        int[] ereArr = new int[max + 1];
+
+        ereArr[0] = 1;
+        ereArr[1] = 1;
+
+        for(int idx = 2; idx < max; idx++){
+            for(int jdx = 2; jdx < max; jdx++){
+                if(idx * jdx <= max){
+                    ereArr[idx * jdx] = 1;
+                }else{
+                    break;
+                }
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(Integer n : nList){
+            int result = 0;
+            for(int jdx = n + 1; jdx <= n * 2; jdx++){
+                if(ereArr[jdx] != 1){
+                    result++;
+                }
+            }
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
