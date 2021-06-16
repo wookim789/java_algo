821a04a63961cbeeb7215c54fa92b76cb9aea9fe
wookim789 <wookim789@gmail.com>
2021-06-16 23:32:08 +0900

    10872 피보나치 수정

--- src/backjoon/recursion/Fibonacci.java
+++ src/backjoon/recursion/Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-1
+
         int n = sc.nextInt();
         sc.close();
 
