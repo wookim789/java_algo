4ab1696df401111919464c7a5f673760151308c8
wookim789 <wookim789@gmail.com>
2021-06-21 23:52:45 +0900

    4153 피타고라스 문제 풀이 완료
    - java stream 다루는 기법을 정리해야함
    - stream를 자유자재로 다루면 파이썬 부럽지 않은듯

--- src/backjoon/mathtwo/Pythagoras.java
+++ src/backjoon/mathtwo/Pythagoras.java
@@ -1,24 +1,45 @@
 package backjoon.mathtwo;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Pythagoras {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         while(true){
             List<Integer> nList = Arrays.stream(br.readLine().split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
 
+            if(nList.stream().mapToInt(x->x).sum() == 0){
+                break;
+            }
+
             int max = nList.stream()
                     .mapToInt(x -> x)
                     .max()
                     .orElseThrow(RuntimeException::new);
+
+            int pythagoras = max * max;
+            for(Integer n : nList){
+                if(n != max){
+                    pythagoras -= n * n;
+                }
+            }
+            if(pythagoras == 0){
+                bw.write("right\n");
+            }else{
+                bw.write("wrong\n");
+            }
         }
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
