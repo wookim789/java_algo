8050a3a5508de54074bd17c32d3f3a6265ebcf67
wookim789 <wookim789@gmail.com>
2021-07-07 23:55:33 +0900

    10989 문제 풀이중5

--- src/backjoon/sort/TreeMapSort2.java
+++ src/backjoon/sort/TreeMapSort2.java
@@ -3,27 +3,33 @@ package backjoon.sort;
 import java.io.*;
 import java.util.*;
 
-//public class Main {
-public class TreeMapSort2 {
+public class Main {
+//public class TreeMapSort2 {
     public static void main(String[]args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.add(Integer.parseInt(br.readLine()));
-//test
         while (n - 1 > 0){
             int ele = Integer.parseInt(br.readLine());
-            int i = linkedList.size() / 2;
-            int lt = 0;
-            int rt = linkedList.size();
+            int lp = 0;
+            int rp = linkedList.size();
+            int i = (lp + rp) / 2;
             while(true){
+                if(lp >= rp){
+                    linkedList.add(lp, ele);
+                    break;
+                }
+
                 if(linkedList.get(i) > ele){
-
+                    rp = i;
+                    i = (lp + rp) / 2;
                 }else if(linkedList.get(i) < ele){
-
+                    lp = i + 1;
+                    i = (lp + rp) / 2;
                 }else{
+                    linkedList.add(i, ele);
                     break;
                 }
             }
