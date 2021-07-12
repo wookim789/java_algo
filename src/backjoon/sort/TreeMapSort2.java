d04cede1f7dd2db1d6b41dd713b47b2e4971fbff
wookim789 <wookim789@gmail.com>
2021-07-12 23:46:12 +0900

    10989 문제 풀이 성공 - 계수 배열 문제 풀이
    - 시간은 조금 길어도 가장 적은 메모리 사용하여 정렬
    - 문제 포인트는 중복되는 숫자들을 정렬하는것
    - 가장 큰수는 10000정도 였다는 것
    - 크기 10000정도의 배열 탐색 시간은 3초 라고 생각하면 될듯

--- src/backjoon/sort/TreeMapSort2.java
+++ src/backjoon/sort/TreeMapSort2.java
@@ -1,47 +1,27 @@
 package backjoon.sort;
 
 import java.io.*;
-import java.util.*;
 
-public class Main {
-//public class TreeMapSort2 {
+public class TreeMapSort2 {
     public static void main(String[]args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        while (n - 1 > 0){
-            int ele = Integer.parseInt(br.readLine());
-            int lp = 0;
-            int rp = linkedList.size();
-            int i = (lp + rp) / 2;
-            while(true){
-                if(lp >= rp){
-                    linkedList.add(lp, ele);
-                    break;
-                }
+        int[] arr = new int[10000 + 1];
 
-                if(linkedList.get(i) > ele){
-                    rp = i;
-                    i = (lp + rp) / 2;
-                }else if(linkedList.get(i) < ele){
-                    lp = i + 1;
-                    i = (lp + rp) / 2;
-                }else{
-                    linkedList.add(i, ele);
-                    break;
-                }
-            }
-            n--;
+        for(int i = 1; i < n + 1; i++){
+            arr[Integer.parseInt(br.readLine())] += 1;
         }
-
-        br.close();
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(Integer x : linkedList){
-            bw.write(x + "\n");
+        for(int i = 1; i < 10000 + 1; i++){
+            while(arr[i] > 0){
+                bw.write(i + "\n");
+                arr[i]--;
+            }
         }
+
         bw.flush();
         bw.close();
     }
