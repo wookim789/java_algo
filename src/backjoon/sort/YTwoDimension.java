5d89916866fa0df9ea14056d91435389a0d5a500
wookim789 <wookim789@gmail.com>
2021-07-25 23:40:06 +0900

    11651 문제 풀이 완료
    - 이전 문제와 동일한 풀이

---
+++ src/backjoon/sort/YTwoDimension.java
@@ -0,0 +1,50 @@
+package backjoon.sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class YTwoDimension {
+    public static void main(String [] agrs) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        SortedSet<Integer> xSet = new TreeSet<>();
+        Map<String, SortedSet<Integer>> yMap = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            String[] strArr = br.readLine().split(" ");
+            int x = Integer.parseInt(strArr[1]);
+            xSet.add(x);
+            String xKey = Integer.toString(x);
+
+            int y = Integer.parseInt(strArr[0]);
+            if(yMap.get(xKey) == null){
+                SortedSet<Integer> ySet = new TreeSet<>();
+                yMap.put(xKey, ySet);
+            }
+            yMap.get(xKey).add(y);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(Integer x : xSet){
+            SortedSet<Integer> ySet = yMap.get(Integer.toString(x));
+            if(ySet != null && !ySet.isEmpty()){
+                for(Integer y : ySet){
+                    bw.write(y + " " + x + "\n");
+                }
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
